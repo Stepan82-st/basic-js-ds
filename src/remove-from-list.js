097dabb5be09d058f44dd7d2c8488e981d95ed6d
Stepan Bryl <bryl.step@tut.by>
2022-08-10 10:30:30 +0300

    feat: nodeList

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -1,6 +1,5 @@
 const { NotImplementedError } = require('../extensions/index.js');
-
-// const { ListNode } = require('../extensions/list-node.js');
+ const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Given a singly linked list of integers l and an integer k,
@@ -22,6 +21,12 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
+ class ListNode {
+     constructor(x) {
+       this.value = x;
+       this.next = null;
+     }
+   }
 function removeKFromList(/* l, k */) {
   throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
